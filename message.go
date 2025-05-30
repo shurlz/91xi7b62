bf7a10378945a7d87926ce4456c3ed6a99880db0
john <hellojohneg@gmail.com>
2025-05-30 02:47:35 +0200

    ref: refactor message event data

--- message.go
+++ message.go
@@ -1,37 +1,12 @@
 package main
 
 import (
-	"errors"
 	"time"
 )
 
-type LogLevelType string
-
-const (
-	ErrorType   LogLevelType = "ERROR"
-	InfoType    LogLevelType = "INFO"
-	MessageType LogLevelType = "MESSAGE"
-	FatalType   LogLevelType = "FATAL"
-	WarningType LogLevelType = "WARNING"
-)
-
-func logLevelIsValid(message interface{}) (bool, error) {
-	switch mssg := message.(type) {
-	case LogLevelType:
-		switch mssg {
-		case ErrorType, InfoType, MessageType, FatalType, WarningType:
-			return true, nil
-		default:
-			return false, errors.New("invalid log level string")
-		}
-	default:
-		return false, errors.New("invalid log level type")
-	}
-}
-
 type LogEventMessage struct {
-	LogLevel LogLevelType
-	Msg      *string
-	Time     time.Time
-	Data     interface{}
+	LogLevel LogLevelType `json:"log_level"`
+	Message  *string      `json:"message"`
+	Time     time.Time    `json:"time"`
+	Data     interface{}  `json:"data"`
 }
